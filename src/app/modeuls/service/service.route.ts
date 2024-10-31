f0b3c4252f0015baf617fa96f7719eae81c0c259
ronyislam502 <ronyislam502@gmail.com>
2024-10-31 18:56:21 +0600

    routes

--- src/app/modeuls/service/service.route.ts
+++ src/app/modeuls/service/service.route.ts
@@ -3,14 +3,14 @@ import { ServiceControllers } from "./service.controller";
 
 const router = express.Router();
 
-router.post("/", ServiceControllers.createService);
+router.post("/create-service", ServiceControllers.createService);
 
 router.get("/", ServiceControllers.getAllServices);
 
-router.get("/:id", ServiceControllers.getSingleService);
+router.get("/service/:id", ServiceControllers.getSingleService);
 
-router.put("/:id", ServiceControllers.updateService);
+router.put("/update/:id", ServiceControllers.updateService);
 
-router.delete("/:id", ServiceControllers.deleteService);
+router.delete("/delete/:id", ServiceControllers.deleteService);
 
 export const ServiceRoutes = router;
